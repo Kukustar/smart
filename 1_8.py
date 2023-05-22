58c759b7f8b1ff1d9f2221e99262eb607977b13d
abuser <friendlymind@yandex.ru>
2023-05-23 01:46:26 +0200

    move 1_8

--- 1_8.py
+++ 1_8.py
@@ -6,7 +6,7 @@ class Node:
 
 
 class LinkedList:
-    
+
     def __init__(self):
         self.head = None
         self.tail = None
@@ -23,7 +23,7 @@ class LinkedList:
         while node != None:
             print(node.value)
             node = node.next
-    
+
     def return_string_all_nodes(self):
         if self.is_empty():
             return ''
@@ -35,7 +35,7 @@ class LinkedList:
             node_string += str(node.value)
             node = node.next
         return node_string    
-    
+
     def is_empty(self):
         return self.head is None        
 
@@ -57,7 +57,7 @@ class LinkedList:
                 nodes.append(current)
             current = current.next
         return nodes
-            
+
     def delete(self, val, all=False):
         if self.is_empty():
             return
@@ -77,7 +77,7 @@ class LinkedList:
                     current.next = current.next.next
             else:
                 current = current.next
-                
+
     def clean(self):
         if self.is_empty():
             return 
@@ -103,85 +103,18 @@ class LinkedList:
         newNode.next = afterNode.next
         afterNode.next = newNode
 
+def sum_linked_lists(list1, list2):
+    if list1.len() != list2.len():
+        return 
 
-def test_insert():
-    my_list = LinkedList()
+    result_list = LinkedList()
+    current1 = list1.head
+    current2 = list2.head
 
-    my_list.add_in_tail(Node(10))
-    my_list.add_in_tail(Node(20))
-    my_list.add_in_tail(Node(30))
+    while current1 and current2:
+        sum_value = current1.value + current2.value
+        result_list.add_in_tail(Node(sum_value))
+        current1 = current1.next
+        current2 = current2.next
 
-    new_node_1 = Node(15)
-    new_node_2 = Node(25)
-    new_node_3 = Node(5)
-
-    after_node_1 = my_list.head
-    my_list.insert(after_node_1, new_node_1)
-
-    after_node_2 = my_list.head.next.next
-    my_list.insert(after_node_2, new_node_2)
-
-    after_node_3 = None
-    my_list.insert(after_node_3, new_node_3)
-        
-    assert my_list.return_string_all_nodes() == '5, 10, 15, 20, 25, 30'
-
-
-def test_find_all():
-    my_list = LinkedList()
-
-    my_list.add_in_tail(Node(10))
-    my_list.add_in_tail(Node(20))
-    my_list.add_in_tail(Node(30))
-    my_list.add_in_tail(Node(20))
-    my_list.add_in_tail(Node(30))
-    
-    nodes = my_list.find_all(20)
-    
-    assert len(nodes) == 2
-    assert nodes[0].value == 20
-    assert nodes[1].value == 20
-        
-      
-def test_clean():
-    my_list = LinkedList()
-
-    my_list.add_in_tail(Node(10))
-    my_list.add_in_tail(Node(20))
-    my_list.add_in_tail(Node(30))
-    my_list.add_in_tail(Node(20))
-    my_list.add_in_tail(Node(30))
-    
-    my_list.clean()
-    
-    assert my_list.head is None
-           
-      
-def test_length():
-    my_list = LinkedList()
-
-    my_list.add_in_tail(Node(10))
-    my_list.add_in_tail(Node(20))
-    my_list.add_in_tail(Node(30))
-    my_list.add_in_tail(Node(20))
-    my_list.add_in_tail(Node(30))
-    
-    assert my_list.len() == 5 
-
-
-def test_delete():
-    my_list = LinkedList()
-
-    my_list.add_in_tail(Node(10))
-    my_list.add_in_tail(Node(20))
-    my_list.add_in_tail(Node(30))
-    my_list.add_in_tail(Node(20))
-    my_list.add_in_tail(Node(30))
-    
-    my_list.delete(10)
-    
-    assert my_list.return_string_all_nodes() == '20, 30, 20, 30'
-    
-    my_list.delete(20, all=True)
-    
-    assert my_list.return_string_all_nodes() == '30, 30'
+    return result_list
